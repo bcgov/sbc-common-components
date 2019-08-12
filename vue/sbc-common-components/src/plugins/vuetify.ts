fbd0a4e5f8e17230a0096d75c9b8c529282b0475
kvangorp <katelyn.vangorp@gmail.com>
2019-08-12 15:18:02 -0700

    Add additonal vuetify components to vuetify.ts

--- vue/sbc-common-components/src/plugins/vuetify.ts
+++ vue/sbc-common-components/src/plugins/vuetify.ts
@@ -2,12 +2,16 @@ import 'material-icons/iconfont/material-icons.css' // Ensure you are using css-
 import 'vuetify/dist/vuetify.min.css'
 import Vue from 'vue'
 import Vuetify, {
-  VAlert
+  VAlert,
+  VContainer,
+  VIcon
 } from 'vuetify/lib'
 
 Vue.use(Vuetify, {
   iconfont: 'md',
   components: {
-    VAlert
+    VAlert,
+    VContainer,
+    VIcon
   }
 })
