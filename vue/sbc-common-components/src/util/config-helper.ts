3475c66275aa3c86ffcf4de83dd98ceea93dc706
Andrew Sutherland <sutherlanda@gmail.com>
2020-03-24 09:55:06 -0700

    Check at session storage root

--- vue/sbc-common-components/src/util/config-helper.ts
+++ vue/sbc-common-components/src/util/config-helper.ts
@@ -25,10 +25,7 @@ export default class ConfigHelper {
 
   static getAuthAPIUrl (): string {
     const apiConfig = JSON.parse(sessionStorage.getItem(SessionStorageKeys.ApiConfigKey) || '{}')
-    if (apiConfig) {
-      return apiConfig['VUE_APP_AUTH_ROOT_API'] || apiConfig['AUTH_API_URL'] || ''
-    }
-    return ''
+    return (apiConfig && apiConfig['VUE_APP_AUTH_ROOT_API']) || sessionStorage.getItem(SessionStorageKeys.AuthApiUrl) || ''
   }
 
   static getAuthContextPath (): string {
