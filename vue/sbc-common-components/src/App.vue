4bb9c68201d75b3cf3f08dbe30a87b84755e1bd9
Munro <Tay.Munro@gov.bc.ca>
2023-06-13 16:32:14 -0700

    starting upgrades

--- vue/sbc-common-components/src/App.vue
+++ vue/sbc-common-components/src/App.vue
@@ -4,15 +4,17 @@
       <sbc-loader :show="false" />
       <sbc-header
         class="flex-column"
-        :key="$store.state.refreshKey"
+        :key="refreshKey"
         :in-auth="true"
         :show-product-selector="false"
-        :show-login-menu="true"
+        :show-login-menu="showLoginMenu"
         @account-switch-started="false"
         @account-switch-completed="false"
         @hook:mounted="setup"
-        ref="header" :redirect-on-logout="logoutUrl">
-        <template v-slot:login-button-text>
+        ref="header"
+        :redirect-on-logout="logoutUrl"
+      >
+        <template #login-button-text>
           Log in with BC Services Card
         </template>
       </sbc-header>
@@ -21,7 +23,7 @@
         color="primary"
         class="mb-6"
         v-model="showNotification"
-        :timeout=6000
+        :timeout="6000"
       >
         <span v-html="notificationText"></span>
         <v-btn
@@ -30,54 +32,30 @@
           color="default"
           aria-label="Close Notification"
           title="Close Notification"
-          @click="showNotification = false">
+          @click="showNotification = false"
+        >
           <v-icon>mdi-close</v-icon>
         </v-btn>
       </v-snackbar>
       <!-- Alert banner -->
       <v-alert
-        tile dense
+        tile
+        dense
         type="warning"
         class="mb-0 text-center color-dk-text"
         v-if="bannerText"
       />
     </div>
-      <div id="nav">
-        <v-row justify="center">
-          <v-col cols="12" md="7">
-            <router-link to="/">Main</router-link> |
-            <router-link to="/AuthMenu">Auth Menu</router-link> |
-            <router-link to="/AuthenticationOptions">Authentication Options</router-link> |
-            <router-link to="/AuthenticationOptionsDialog">Authentication Options Dialog</router-link> |
-            <router-link to="/BaseAddress">Base Address</router-link> |
-            <router-link to="/BrowserVersionAlert">Browser Version Alert</router-link> |
-            <router-link to="/FeeSummary">Fee Summary</router-link> |
-            <router-link to="/LoadingScreen">Loading Screen</router-link> |
-            <router-link to="/Loader">Loader</router-link> |
-            <router-link to="/Login">Login</router-link> |
-            <router-link to="/MobileDeviceAlert">Mobile Device Alert</router-link> |
-            <router-link to="/NavigationBar">Navigation Bar</router-link> |
-            <router-link to="/NotificationPanel">Notification Panel</router-link> |
-            <router-link to="/PaySystemAlert">Pay System Alert</router-link> |
-            <router-link to="/ProductSelector">Product Selector</router-link> |
-            <router-link to="/SignIn">Sign In</router-link> |
-            <router-link class="disabled" to="/SignOut">Sign Out</router-link> |
-            <router-link to="/SystemAlert">System Alert</router-link> |
-            <router-link to="/SystemBanner">System Banner</router-link> |
-            <router-link to="/SystemError">System Error</router-link> |
-            <router-link to="/SystemErrorModal">System Error Modal</router-link>
-          </v-col>
-        </v-row>
-      </div>
     <div class="app-body">
       <router-view />
     </div>
-    <sbc-footer :aboutText='aboutText'></sbc-footer>
+    <sbc-footer :aboutText="aboutText"></sbc-footer>
   </v-app>
 </template>
 
 <script lang="ts">
-import { Component, Mixins, Vue } from 'vue-property-decorator'
+import { defineComponent } from 'vue'
+import { mapActions, mapGetters, mapMutations, mapState } from 'vuex'
 import SbcFooter from '/src/components/SbcFooter.vue'
 import SbcHeader from '/src/components/SbcHeader.vue'
 import SbcLoader from '/src/components/SbcLoader.vue'
@@ -85,11 +63,11 @@ import KeyCloakService from '../src/services/keycloak.services'
 import { LDFlags, Pages, SessionStorageKeys } from '@/util/constants'
 import { appendAccountId } from '../src/util/common-util'
 import { getModule } from 'vuex-module-decorators'
-import { mapActions, mapGetters, mapMutations, mapState } from 'vuex'
 import AuthModule from '../src/store/modules/auth'
 import { KCUserProfile } from '../src/models/KCUserProfile'
 import LaunchDarklyService from '../src/services/launchdarkly.services'
-@Component({
+
+export default defineComponent({
   components: {
     SbcHeader,
     SbcFooter,
@@ -101,135 +79,107 @@ import LaunchDarklyService from '../src/services/launchdarkly.services'
       'permissions'
     ]),
     ...mapState('user', ['currentUser']),
-    ...mapGetters('auth', ['isAuthenticated'])
+    ...mapGetters('auth', ['isAuthenticated']),
+    showNavigationBar () {
+      return this.$route.meta.showNavBar
+    },
+    showLoginMenu () {
+      return this.$route.path !== `/${Pages.LOGIN}`
+    },
+    bannerText () {
+      const bannerText = LaunchDarklyService.getFlag(LDFlags.BannerText)
+      return bannerText?.trim() || null
+    },
+    aboutText () {
+      return process.env.ABOUT_TEXT
+    }
   },
   methods: {
     ...mapMutations('org', ['setCurrentOrganization']),
     ...mapActions('user', ['loadUserInfo'])
-  }
-})
-export default class App extends Mixins() {
-  authModule = getModule(AuthModule, this.$store)
-  readonly loadUserInfo!: () => KCUserProfile
-  showNotification = false
-  notificationText = ''
-  showLoading = true
-  toastType = 'primary'
-  toastTimeout = 6000
-  logoutUrl = ''
-
-  $refs: {
-    header: SbcHeader
-  }
-
-  get showNavigationBar (): boolean {
-    return this.$route.meta.showNavBar
-  }
-
-  get showLoginMenu (): boolean {
-    // Don't show the login menu if the user is on login page
-    return this.$route.path !== `/${Pages.LOGIN}`
-  }
-
-  get bannerText (): string | null {
-    const bannerText: string = LaunchDarklyService.getFlag(LDFlags.BannerText)
-    // remove spaces so that " " becomes falsy
-    return bannerText?.trim()
-  }
-
-  /** The About text. */
-  get aboutText (): string {
-    return process.env.ABOUT_TEXT
-  }
-
-  startAccountSwitch () {
-    this.showLoading = true
-  }
-
-  async setup (isSigninComplete?: boolean) {
-    // Header added modules to store so can access mapped actions now
-    if (this.$store.getters['auth/isAuthenticated']) {
-      try {
-        if (!isSigninComplete) {
-          await KeyCloakService.initializeToken(this.$store)
-        }
-        this.loadUserInfo()
-      } catch (e) {
-        // eslint-disable-next-line no-console
-        console.log('App.vue.setup Error: ' + e)
-        this.$store.dispatch('user/reset')
-        this.$store.commit('loadComplete')
-        this.$router.push('/home')
-      }
+  },
+  data () {
+    return {
+      showNotification: false,
+      notificationText: '',
+      showLoading: true,
+      toastType: 'primary',
+      toastTimeout: 6000,
+      logoutUrl: ''
     }
-    this.$store.commit('loadComplete')
-  }
-  public async mounted (): Promise<void> {
+  },
+  mounted () {
     sessionStorage.setItem(SessionStorageKeys.StatusApiUrl, 'https://status-api-dev.apps.silver.devops.gov.bc.ca/api/v1')
     this.showLoading = false
   }
-}
+})
 </script>
+
 <style lang="scss">
-#nav{
+#nav {
   padding: 5px;
   text-align: justify;
   text-align-last: center;
   z-index: 100;
-  a{
+
+  a {
     font-weight: bold;
     color: #adb5bd;
-    &.router-link-exact-active{
+
+    &.router-link-exact-active {
       color: #003366;
     }
-    &:hover{
+
+    &:hover {
       color: #2de0a5;
     }
   }
 }
-.navCol{
+
+.navCol {
   padding: 10px;
   text-align: justify;
   text-align-last: center;
 }
 
 .app-container {
-    display: flex;
-    flex-flow: column nowrap;
-    min-height: 100vh
-  }
-
-  .header-group {
-    position: sticky;
-    position: -webkit-sticky; /* For Safari support */
-    top: 0;
-    width: 100%;
-    z-index: 2;
-  }
-  .pageTitle{
-    font-size: 3rem;
-    font-weight: bold;
-    color: #003366;
-    text-align: justify;
-    text-align-last: center;
-    padding-bottom: 20px;
-    margin-bottom: 20px;
-    text-transform: uppercase;
-    text-decoration: underline;
-    text-decoration-color: #fcba19;
-    justify-content: center;
-    align-items: center;
-    z-index: 5;
-  }
-
-  .app-body {
-    flex: 1 1 auto;
-    position: relative;
-  }
-  .disabled {
-    pointer-events: none;
-    background: red;
-    opacity: 0.24;
+  display: flex;
+  flex-flow: column nowrap;
+  min-height: 100vh;
 }
 
+.header-group {
+  position: sticky;
+  position: -webkit-sticky; /* For Safari support */
+  top: 0;
+  width: 100%;
+  z-index: 2;
+}
+
+.pageTitle {
+  font-size: 3rem;
+  font-weight: bold;
+  color: #003366;
+  text-align: justify;
+  text-align-last: center;
+  padding-bottom: 20px;
+  margin-bottom: 20px;
+  text-transform: uppercase;
+  text-decoration: underline;
+  text-decoration-color: #fcba19;
+  justify-content: center;
+  align-items: center;
+  z-index: 5;
+}
+
+.app-body {
+  flex: 1 1 auto;
+  position: relative;
+}
+
+.disabled {
+  pointer-events: none;
+  background: red;
+  opacity: 0.24;
+}
 </style>
--- vue/sbc-common-components/src/services/document.services.ts
+++ vue/sbc-common-components/src/services/document.services.ts
@@ -1,10 +1,9 @@
 import { AxiosResponse } from 'axios'
 import ConfigHelper from '@/util/config-helper'
-import { TermsOfUseDocument } from '@/models/TermsOfUseDocument'
 import { axios } from '../../src/util/http-util'
 
 export default class DocumentService {
-  static async getTermsOfService (identifier: string): Promise<AxiosResponse<TermsOfUseDocument>> {
+  static async getTermsOfService (identifier: string): Promise<AxiosResponse> {
     return axios.get(`${ConfigHelper.getAuthAPIUrl()}/documents/${identifier}`)
   }
 }
