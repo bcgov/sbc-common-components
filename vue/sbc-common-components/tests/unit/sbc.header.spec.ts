798736794337381d49867497d0f05d8137ecbb0b
Munro <Tay.Munro@gov.bc.ca>
2023-07-14 16:45:17 -0700

    Everything working again + more tests

--- vue/sbc-common-components/tests/unit/sbc.header.spec.ts
+++ vue/sbc-common-components/tests/unit/sbc.header.spec.ts
@@ -2,6 +2,7 @@ import { mount } from '@vue/test-utils';
 import SbcHeader from '@/components/SbcHeader.vue';
 import vuetify from './setup';
 import { createStore } from 'vuex';
+import { createVueRouter } from './setup';
 import AccountModule from '@/store/modules/account';
 import AuthModule from '@/store/modules/auth';
 import NotificationModule from '@/store/modules/notification'
@@ -9,6 +10,7 @@ import NotificationModule from '@/store/modules/notification'
 
 describe('SbcHeader', () => {
     let store: any;
+    let router: any;   
 
     beforeEach(() => {
         // Create a new Vuex store instance with the required modules
@@ -19,14 +21,18 @@ describe('SbcHeader', () => {
             notification: NotificationModule
           },
         });
+        router = createVueRouter();
+
     });
   it('renders the brand title correctly', () => {
     const wrapper = mount(SbcHeader, {
         global: {
-            plugins: [store, vuetify]
+            plugins: [store, vuetify, router]
         }
     });
     const header = wrapper.find('.app-header');
     expect(header.exists()).toBe(true);
   });
+
+  //add more comprehensive tests here
 });
