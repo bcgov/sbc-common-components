2caeb9ca7c58d0af528df2bf906f94ec250044b4
Travis Semple <travis8814@gmail.com>
2025-03-05 11:43:34 -0800

    Remove junk

--- vue/sbc-common-components/src/views/Home.vue
+++ vue/sbc-common-components/src/views/Home.vue
@@ -6,12 +6,5 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import HelloWorld from '../components/HelloWorld.vue'
-
-@Component({
-  components: {
-    HelloWorld
-  }
-})
 export default class Home extends Vue {}
 </script>
